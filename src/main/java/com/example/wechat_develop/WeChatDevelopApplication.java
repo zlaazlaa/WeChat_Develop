1302eff3d7ca5a772c0a4b334cf610eda9dba0b3
ZLAA <72907523+zlaazlaa@users.noreply.github.com>
2022-08-13 22:43:34 +0800

    Initial commit

---
+++ src/main/java/com/example/wechat_develop/WeChatDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.wechat_develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeChatDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeChatDevelopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wechat_develop/WeChatDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wechat_develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeChatDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
